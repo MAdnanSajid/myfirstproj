8754abf75793e75889dcb899455fbb661a38ee9f
adnan <adnan.sajid5017@gmail.com>
2024-08-01 00:41:24 +0500

    Completed.

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,20 +1,43 @@
-import { useEffect, useState } from "react";
+import React, {useState} from "react";
+import Logo from "../assests/pizzaLogo.png";
+import {Link} from "react-router-dom";
+import ReorderIcon from '@mui/icons-material/Reorder';
+import "../styles/Navbar.css";
+function Navbar() {
 
+  const [openLinks,setOpenLinks] = useState(false);
 
-const Navbar = () => {
+  const toggleNavbar = () => {
 
-    const [value, setValues] = useState(10);
+    setOpenLinks(!openLinks);
+  };
 
-    const setValues1 = () =>{
-        setValues(200);
-    }
-    
-    return (
-        <div>
-            True Values {value}
-            <button onClick={setValues1}>press me</button>
+  return (
+    <div className="navbar" >
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={Logo}/>
+        <div className="hiddenLinks">
+        <Link to="/"> Home</Link>
+        <Link to="/menu"> Menu</Link>
+        <Link to="/about"> About</Link>
+        <Link to="/contact"> Contact</Link>
         </div>
-    )
+
+      </div>
+      <div className="rightSide">
+        <Link to="/"> Home</Link>
+        <Link to="/menu"> Menu</Link>
+        <Link to="/about"> About</Link>
+        <Link to="/contact"> Contact</Link>
+        <button onClick={toggleNavbar}>
+            <ReorderIcon/>
+        </button>
+      
+
+
+      </div>
+    </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
